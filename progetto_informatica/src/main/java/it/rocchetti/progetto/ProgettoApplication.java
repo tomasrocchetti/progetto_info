f65fd2fb1945fe597e18d5224d683303a760d2d9
Tomas <tomas.rocchetti@gmail.com>
2020-02-09 15:45:25 +0100

    a

---
+++ progetto_informatica/src/main/java/it/rocchetti/progetto/ProgettoApplication.java
@@ -0,0 +1,25 @@
+package it.rocchetti.progetto;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages={"it.rocchetti"})
+public class ProgettoApplication {
+
+	public final static String LINK = new String("https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/isoc_ec_eseln2.tsv.gz&unzip=true");
+	public final static String PATH = new String("j.tsv");
+
+	public static void main(String[] args) throws Exception {
+
+		
+		
+	}
+
+}
---
+++ progetto_informatica/src/test/java/it/rocchetti/progetto/ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.rocchetti.progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
