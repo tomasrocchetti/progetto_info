6c6f8925370583fb53a53489505da99b457604c1
Tomas <tomas.rocchetti@gmail.com>
2020-02-09 17:44:51 +0100

    JavaDoc

---
+++ progetto_informatica/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"it.rocchetti.model","l":"DataModel"}]
--- progetto_informatica/src/main/java/it/rocchetti/model/DataModel.java
+++ progetto_informatica/src/main/java/it/rocchetti/model/DataModel.java
@@ -7,8 +7,11 @@ public class DataModel {
 	
 	
 	/** crea il modello per i dati:
+	 * <p>
 	 * - id per determinare il numero della riga (id = 0 -> intestazione)
+	 * <p>
 	 * - description, attributi in una sola string (parsing separato)
+	 * <p>
 	 * - value, insieme dei valori, max su 200 anni
 	**/
 	
@@ -17,24 +20,65 @@ public class DataModel {
 		this.description = dataType;
 		this.value = value;
 	}
+ 	
+ 	/**
+ 	 * Ritorna l'ID della riga
+ 	 * @return id della riga
+ 	 */
  	public int getId() {
 		return id;
 	}
+ 	/**
+ 	 * Setta l'ID della riga
+ 	 * @param mId
+ 	 * 
+ 	 * 
+ 	 */
  	public void setId(int mId) {
 		this.id = mId;
 	}
+ 	/**
+ 	 * Ritorna la descrizione
+ 	 * @return description
+ 	 * 
+ 	 * 
+ 	 */
 	public String getDescription() {
 		return description;
 	}
+ 	/**
+ 	 * Setta la descrizione
+ 	 * @param dataType
+ 	 * 
+ 	 * 
+ 	 */
 	public void setDescription(String dataType) {
 		this.description = dataType;
 	}
+ 	/**
+ 	 * Ritorna l'array di valori 
+ 	 * @return value[id]
+ 	 * 
+ 	 * 
+ 	 */
 	public int getValue(int id) {
 		return value[id];
 	}
+ 	/**
+ 	 * Setta il valore dell'array dato l'indice
+ 	 * @param value1, index
+ 	 * 
+ 	 * 
+ 	 */
 	public void setValue(int value1, int index) {
 		this.value[index] = value1;
 	}
+ 	/**
+ 	 * Ritorna la lunghezza dell'array
+ 	 * @return value.lenght
+ 	 * 
+ 	 * 
+ 	 */
 	public int getSize() {
 		return value.length;
 	}
