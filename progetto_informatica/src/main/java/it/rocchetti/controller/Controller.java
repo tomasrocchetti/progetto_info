755a039452d9085489bed245bd01b6aa4b7c83f3
Tomas <tomas.rocchetti@gmail.com>
2020-02-10 09:16:34 +0100

    implementato metodo per ritornare lista nazioni

--- progetto_informatica/src/main/java/it/rocchetti/controller/Controller.java
+++ progetto_informatica/src/main/java/it/rocchetti/controller/Controller.java
@@ -1,5 +1,13 @@
 package it.rocchetti.controller;
 
-public class Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Controller {
+	@GetMapping("/country")
+	public String country() throws Exception {
+		return "";
+	}
 }
